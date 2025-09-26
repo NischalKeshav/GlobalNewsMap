f1d6817dd0feebb7d21dfa920f8984b953d45da9
Arjun Paradkar <158537650+ArjunParadkar@users.noreply.github.com>
2025-09-25 19:22:07 -0500

    Update newsGrabber.py
    Embarrassing ...uhhh just input ur own API key...

--- newsGrabber.py
+++ newsGrabber.py
@@ -1,4 +1,4 @@
 from newsapi import NewsApiClient
-newsapi = NewsApiClient(api_key='4bb8b4df3f1a4b92925eafa0c4b22845')
+newsapi = NewsApiClient(api_key= "Enter API Key Here")
 top_headlines = newsapi.get_top_headlines(country='us')
 print(top_headlines)
